5907a820ad757f43aece87c66048de216bedfaad
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-25 16:20:18 +0530

    add foods

---
+++ src/components/Foodlist.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Foodlist() {
+    return(
+        <React.Fragment>
+            <ul>
+                <li>Name - Kottu -indridies- chili</li>
+                <li>Name - Egg Rice -indridies- Rice,Egg</li>
+                <li>Name - pittu -indridies- coconut</li>
+
+            </ul>
+        </React.Fragment>
+    )
+}
+
+export default Foodlist;
