e849212efe81d2ff35399cda0425c254b0ecaadb
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-25 15:50:58 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import React, {Component} from 'react';
-import './App.css'
-import Form from './components/Form.jsx';
+import Form from './components/Form.jsx'
 
 function App() {
-  getrecipe() {
-    console.log("Working!!!");
-  }
-  render() {
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Recipe App</h1>
-      </header>
-      <Form getrecipe={this.getrecipe} />
+    <div>
+    
+      
     </div>
-  );
-  };
-};
+  )
+  
+}
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 
 
-const Form = props => (
-    <form onSubmit={props.getrecipe}>
-        <input type="text"/>
-        <button>Search</button>
 
-
-    </form>
-);
 
 export default Form;
