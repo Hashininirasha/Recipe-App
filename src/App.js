2c2d496a037646b347fa763f8cbad7ab7268c58f
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-26 12:18:37 +0530

    add mainjs and header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
+import './App.css'
 import Foodlist from './components/Foodlist.jsx';
 
 function App() {
   
  
   return (
-    <div>
-  
-
-
-    <Foodlist/>
+    <div className="App">
+      <h1><div class="p-3 mb-2 bg-primary text-white">Food Recipe App</div></h1>
     </div>
     
   )
