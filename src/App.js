e30c4f131d4d9824d720d24dafec0ee94e578032
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-26 17:48:20 +0530

    add react dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
+import Axios from 'axios';
 import './App.css'
-import Foodlist from './components/Foodlist.jsx';
 
 
 const App = () =>{
@@ -8,10 +8,13 @@ const App = () =>{
   const APP_KEY="b5c9bab94750b4d7343f70237d7b5a9d";
   const url = 'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}';
 
- 
+  const getData = async () => {
+    const result = await Axios.get(url);
+    console.log(result)
+  }
   return (
     <div className="App">
-      <h1><div class="p-3 mb-2 bg-primary text-white">Food Recipe App</div></h1>
+      <h1 onClick={getData}><div class="p-3 mb-2 bg-primary text-white">Food Recipe App</div></h1>
     </div>
     
   )
--- src/components/recipe.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function recipe() {
-    return
-        <li>Name - Kottu -indridies- chili</li>
-    
-}
-
-export default recipe;
