7c233f5318581eff4292456fc788113a333a1771
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-27 15:30:04 +0530

    add buttons

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import './App.css'
 
 
 const App = () =>{
-  const APP_ID="5661db5d"
+  const APP_ID="5661db5d";
   const APP_KEY="b5c9bab94750b4d7343f70237d7b5a9d";
   const url = 'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}';
 
   const getData = async () => {
     const result = await Axios.get(url);
-    console.log(result)
-  }
+    console.log(result);
+  };
   return (
     <div className="App">
-      <h1 onClick={getData}><div class="p-3 mb-2 bg-primary text-white">Food Recipe App</div></h1>
+      <h1 onClick={getData}><div class="p-3 mb-2 bg-primary text-white">Food Recipe App</div></h1><br/>
+      <form className="search-form">
+        <input class="form-control" type="text" placeholder="Search Food" autoComplete="off"/>
+        <input class="btn btn-outline-success my-2 my-sm-0" type="submit" value="Search"/>
+      </form>
     </div>
     
   )
